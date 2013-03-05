86297639e1c4bed2c0c733d4dd8c4141b3887341
ericminio <eric.mignot@gmail.com>
2013-03-05 07:36:50 -0500

    Pretend the home page offers to display the prime factors of a number

--- spec/decomposition.spec.js
+++ spec/decomposition.spec.js
@@ -4,22 +4,17 @@ describe("decomposition", function() {
 
 	var home = "http://localhost:5000/index.html";
 	var browser = new Browser();
-
-	it("displays the decomposition of the input number", function(done) {
-		browser.visit(home).
-			then(function () {
-				browser.fill("#number", "30");
-				return browser.clickLink("#decompose");
-			}).
-			then(function() {
+	
+	it("offers to display the prime factors of a number", function(done) {
+		browser.visit(home, function () {
+			browser.fill("#number", "30");
+			browser.clickLink("#decompose", function() {
 				expect(browser.text("#decomposition")).toEqual("30 = 2 x 3 x 5");
 				done();
-			}).
-			fail(function(error) {
-				console.log(error);
 			});
+		});
 	});		
-	
+
 });
 		
 		
