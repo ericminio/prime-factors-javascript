24b71b9a09da62cce4b312fc7d0d57adba731f57
ericminio <eric.mignot@gmail.com>
2013-03-06 10:58:00 -0500

    … a server serves public/index.html

--- spec/decomposition.spec.js
+++ spec/decomposition.spec.js
@@ -1,10 +1,18 @@
 Browser = require("zombie");
+var serving = require('../public/server');
 
 describe("decomposition", function() {
 
 	var home = "http://localhost:5000/index.html";
 	var browser = new Browser();
 	
+	beforeEach(function() {	
+		server = require('http').createServer(serving('public')).listen(5000, 'localhost');		
+	});
+	afterEach(function() {
+		server.close();
+	});	
+		
 	it("offers to display the prime factors of a number", function(done) {
 		browser.visit(home, function () {
 			browser.fill("#number", "30");
